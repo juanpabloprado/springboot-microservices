529386becf3ab219189fc895ab00ec8f37935c22
Juan Prado <juan@juanpabloprado.com>
2024-06-27 19:17:15 -0600

    add eureka service registry

--- employee-service/src/main/java/com/juanpabloprado/employeeservice/department/DepartmentClient.java
+++ employee-service/src/main/java/com/juanpabloprado/employeeservice/department/DepartmentClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "departments", url = "localhost:8080")
+@FeignClient("department-service")
 public interface DepartmentClient {
     @GetMapping("api/departments/{code}")
     DepartmentResponse getDepartment(@PathVariable String code);
